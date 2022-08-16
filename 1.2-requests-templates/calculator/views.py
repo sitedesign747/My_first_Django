b4d97a80d35b361db6cfc2b20e9efa4a14c7c68f
sitedesign747 <63049450+sitedesign747@users.noreply.github.com>
2022-08-16 15:34:42 +0600

    Add Files via upload
    Add changed Files via upload

---
+++ 1.2-requests-templates/calculator/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render, reverse
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+    # можете добавить свои рецепты ;)
+}
+
+
+# Напишите ваш обработчик. Используйте DATA как источник данных
+# Результат - render(request, 'calculator/index.html', context)
+#  качестве контекста должен быть передан словарь с рецептом:
+# context = {
+#  'recipe': {
+#     'ингредиент1': количество1,
+#     'ингредиент2': количество2,
+#   }
+# }
+
+# context и параметры render менять не нужно
+# подбробнее о них мы поговорим на следующих лекциях
+
+def omlet(request):
+    servings = int(request.GET.get("servings", 1))
+    context = {
+        'recipe': {
+        'яйца, шт': 2 * servings,
+        'молоко, л': 0.1 * servings,
+        'соль, ч.л.': 0.5 * servings,
+        }
+    }
+    return render(request, 'calculator/index.html', context)
+
+
+def pasta(request):
+    servings = int(request.GET.get("servings", 1))
+    context = {
+        'recipe': {
+            'макароны, г': 0.3 * servings,
+            'сыр, г': 0.05 * servings,
+        },
+    }
+    return render(request, 'calculator/index.html', context)
+
+def buter(request):
+    servings = int(request.GET.get("servings", 1))
+    context = {
+        'recipe': {
+            'хлеб, ломтик': 1 * servings,
+            'колбаса, ломтик': 1 * servings,
+            'сыр, ломтик': 1 * servings,
+            'помидор, ломтик': 1 * servings,
+        },
+    }
+
+    return render(request, 'calculator/index.html', context)
+
+
